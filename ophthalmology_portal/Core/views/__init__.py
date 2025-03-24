2895710f6d304a2830d1162fba54eefa619fb380
Loganp344 <loganp3444@gmail.com>
2025-03-24 01:02:34 +0000

    fixed a bug

--- ophthalmology_portal/Core/views/__init__.py
+++ ophthalmology_portal/Core/views/__init__.py
@@ -1,20 +1,20 @@
+from ophthalmology_portal.Core.views.daily_exam_view import DailyExamsView
 from ophthalmology_portal.Core.views.exam_creation_view import ExamCreationView
+from ophthalmology_portal.Core.views.exam_data_submission_view import (
+    TestInformationCreationView,
+)
+from ophthalmology_portal.Core.views.exam_details_view import ExamDetailsView
 from ophthalmology_portal.Core.views.home_page_view import HomePageView
 from ophthalmology_portal.Core.views.log_in_view import (
     LogInView,
     PatientRegistrationView,
     RegistrationView,
 )
+from ophthalmology_portal.Core.views.patient_history_view import PatientExamHistoryView
+from ophthalmology_portal.Core.views.patient_list_view import PatientListView
 from ophthalmology_portal.Core.views.prescription_creation_view import (
     PrescriptionCreationView,
 )
-from ophthalmology_portal.Core.views.test_information_creation_view import (
-    TestInformationCreationView,
-)
-from ophthalmology_portal.Core.views.patient_list_view import PatientListView
-from ophthalmology_portal.Core.views.patient_history_view import PatientExamHistoryView
-from ophthalmology_portal.Core.views.exam_details_view import ExamDetailsView
-from ophthalmology_portal.Core.views.daily_exam_view import DailyExamsView
 
 __all__ = [
     "HomePageView",
@@ -27,5 +27,5 @@ __all__ = [
     "PatientListView",
     "PatientExamHistoryView",
     "ExamDetailsView",
-    "DailyExamsView"
+    "DailyExamsView",
 ]
