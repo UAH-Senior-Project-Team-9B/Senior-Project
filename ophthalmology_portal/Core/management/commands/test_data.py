3f4378d32c52826b2cb44b89a3b0bf8727b40553
Loganp344 <loganp3444@gmail.com>
2025-03-25 00:19:10 +0000

    polished up script

--- ophthalmology_portal/Core/management/commands/test_data.py
+++ ophthalmology_portal/Core/management/commands/test_data.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import (
+    Group,
+    Permission,
+)
 from django.core.management.base import BaseCommand
 
 from ophthalmology_portal.Core.forms.base_user_form import BaseUserForm
@@ -12,6 +15,22 @@ class Command(BaseCommand):
     help = "Generates data used for debugging. DO NOT RUN IN DEVELOPMENT."
 
     def handle(self, *args, **options):
+        patient_group, patient_exists = Group.objects.get_or_create(name="Patients")
+        manager_group, manager_exists = Group.objects.get_or_create(
+            name="Office Manager"
+        )
+        doctor_group, doctor_exists = Group.objects.get_or_create(
+            name="Ophthalmologist"
+        )
+        if patient_exists:
+            for permission in Permission.objects.filter(name="Patient Permissions"):
+                patient_group.permissions.add(permission)
+        if manager_exists:
+            for permission in Permission.objects.filter(name="Manager Permissions"):
+                manager_group.permissions.add(permission)
+        if doctor_exists:
+            for permission in Permission.objects.filter(name="Doctor Permissions"):
+                doctor_group.permissions.add(permission)
         user_form = BaseUserForm({"username": "manager", "password": "1234"})
         user = user_form.save(commit=False)
         user.set_password(user_form.cleaned_data["password"])
