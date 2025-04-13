3eaa0ce2df02571f2e6b58b8efba4f4af2d5d187
Loganp344 <loganp3444@gmail.com>
2025-04-13 20:27:35 +0000

    Exam Confirmations changes

--- ophthalmology_portal/Core/views/patient_history_view.py
+++ ophthalmology_portal/Core/views/patient_history_view.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.http import HttpRequest
+from django.http import HttpRequest, Http404
 from django.shortcuts import render
 
 from ophthalmology_portal.Core.models import ExamModel, PatientUserModel
@@ -7,11 +7,19 @@ from ophthalmology_portal.Core.views.base_view import BaseView
 
 class PatientExamHistoryView(BaseView):
     def get(self, request: HttpRequest, patient=None, *args, **kwargs):
+        # Security needed
         if not patient:
-            patient = request.user.patientusermodel.id
+            if self.patient_verification(request.user):
+                patient = request.user.patientusermodel.id
+            else:
+                raise Http404
+        else:
+            if not self.manager_verification(request.user) and not self.doctor_verification(request.user):
+                raise Http404
         exams = ExamModel.objects.filter(
             patient=PatientUserModel.objects.get(id=patient)
         )
+        patient_obj = PatientUserModel.objects.get(id=patient)
         paginator = Paginator(exams, 10)
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
@@ -21,6 +29,7 @@ class PatientExamHistoryView(BaseView):
             {
                 "page_obj": page_obj,
                 "patient": patient,
+                "patient_obj": patient_obj,
                 "base_template_name": self.get_base_template(request.user),
             },
         )
