54f9588bb132c0821208f5e51394fa42c036caf1
Loganp344 <loganp3444@gmail.com>
2025-04-14 16:50:22 +0000

    Added Search Bar

--- ophthalmology_portal/Core/views/patient_list_view.py
+++ ophthalmology_portal/Core/views/patient_list_view.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from django.db.models import Q
 from ophthalmology_portal.Core.models import PatientUserModel
 from ophthalmology_portal.Core.views.base_view import BaseView
 from django.http import Http404
@@ -10,13 +11,18 @@ class PatientListView(BaseView):
     def get(self, request: HttpRequest, *args, **kwargs):
         if not self.manager_verification(request.user) and not self.doctor_verification(request.user):
             raise Http404
-        patients = PatientUserModel.objects.all()
+        if "HX-target" in request.headers:
+            patients = PatientUserModel.objects.filter(Q(first_name__icontains=request.GET['search'])|Q(last_name__icontains=request.GET['search']))
+            template_name="patient_list_element.html"
+        else:
+            patients = PatientUserModel.objects.all()
+            template_name="patient_list.html"
         paginator = Paginator(patients, 10)
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
         return render(
             request,
-            "patient_list.html",
+            template_name,
             {
                 "page_obj": page_obj,
                 "base_template_name": self.get_base_template(request.user),
