6382c17d8987dd61f46c50f90ec4140612deeb3e
notsuicuu <79546248+notsuicuu@users.noreply.github.com>
2025-04-14 00:21:22 -0500

    Duct Tape

--- ophthalmology_portal/Core/views/cancel_exam.py
+++ ophthalmology_portal/Core/views/cancel_exam.py
@@ -1,28 +1,31 @@
-import datetime
-from zoneinfo import ZoneInfo
-
-from django.core.paginator import Paginator
-from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.urls import reverse
+
 from ophthalmology_portal.Core.models import ExamModel
 from ophthalmology_portal.Core.models.user_models import PatientUserModel
 from ophthalmology_portal.Core.views.base_view import BaseView
-from django.shortcuts import redirect
+
 
 class CancelExam(BaseView):
-    def get(self, request, exam_id, *args, **kwargs):
-        if not self.manager_verification(request.user) and not self.patient_verification(request.user):
+    def get(self, request, exam_id, return_page, *args, **kwargs):
+        if not self.manager_verification(
+            request.user
+        ) and not self.patient_verification(request.user):
             raise Http404
         exam = ExamModel.objects.get(id=exam_id)
         if request.user.has_perm("Core.patient"):
             try:
-                exam = ExamModel.objects.get(id=exam_id, patient=PatientUserModel.objects.get(user=request.user))
+                exam = ExamModel.objects.get(
+                    id=exam_id, patient=PatientUserModel.objects.get(user=request.user)
+                )
             except:
                 raise Http404
-        if exam.status != ExamModel.status_choices['complete']:
+        if exam.status != ExamModel.status_choices["complete"]:
             exam.delete()
         else:
             raise Http404
-        return redirect(reverse("home_page"))
+        if return_page == "True":
+            return redirect(request.META.get("HTTP_REFERER"))
+        else:
+            return redirect(reverse("home_page"))
